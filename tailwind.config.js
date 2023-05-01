ac84580cc374ed7a32b46c6db717af3950eed99d
Jameshw19 <108671331+Jameshw19@users.noreply.github.com>
2023-05-01 15:11:50 +0100

    updated purge in tailwindconfig to whitelist

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  purge: {
+    enabled: process.env.NODE_ENV === "production",
+    content: [
+      "./app/**/*.{js,ts,jsx,tsx}",
+      "./pages/**/*.{js,ts,jsx,tsx}",
+      "./components/**/*.{js,ts,jsx,tsx}",
+    ],
+    defaultExtractor: (content) => {
+      const matches = content.match(/[\w-/:]+(?<!:)/g) || [];
+      return matches;
+    },
+    whitelist: ["bg-red-500", "text-blue-900"],
+  },
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
