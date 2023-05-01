7393d55037ebdb86cd5c2f1bc3c8b294c13d1af2
Jameshw19 <108671331+Jameshw19@users.noreply.github.com>
2023-05-01 15:17:00 +0100

    reverted tailwindconfig back to normal and added the devDependencies into dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: {
-    enabled: process.env.NODE_ENV === "production",
-    content: [
-      "./app/**/*.{js,ts,jsx,tsx}",
-      "./pages/**/*.{js,ts,jsx,tsx}",
-      "./components/**/*.{js,ts,jsx,tsx}",
-    ],
-    defaultExtractor: (content) => {
-      const matches = content.match(/[\w-/:]+(?<!:)/g) || [];
-      return matches;
-    },
-    whitelist: ["bg-red-500", "text-blue-900"],
-  },
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
@@ -21,9 +8,5 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [
-    require("tailwind-scrollbar-hide"),
-    require("tailwindcss"),
-    require("autoprefixer"),
-  ],
+  plugins: [require("tailwind-scrollbar-hide")],
 };
