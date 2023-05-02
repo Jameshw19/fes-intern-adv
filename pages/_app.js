d51785cb49915275836b60a00ad3d9628c964b9c
Jameshw19 <108671331+Jameshw19@users.noreply.github.com>
2023-05-02 12:13:21 +0100

    added @mui to _appjs

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,8 @@
 import "@/styles/globals.css";
+import "@mui/base";
+import "@mui/icons-material";
+import "@mui/material";
+
 import { AuthContextProvider } from "../Components/context/AuthContext";
 
 export default function App({ Component, pageProps }) {
